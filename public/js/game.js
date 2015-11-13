6989679d6f66580a12f736d126aff6d50cb03ed8
Jon Kuperman <jon.kuperman@gmail.com>
2015-11-12 21:23:50 -0800

    fixing bugs

--- public/js/game.js
+++ public/js/game.js
@@ -1,29 +1,30 @@
-(function() {
-  var socket = io.connect('http://localhost:3000');
+var socket = io.connect('http://localhost:3000');
 
-  socket.on('currentUsers', function (data) {
-    $('#users').html('Current Users: ' + data.users);
+socket.on('currentUsers', function (data) {
+  $('#users').html("Current users: " + data.users);
+});
+
+socket.on('currentCelebs', function(data) {
+  data.celebs.forEach(function(celeb) {
+    $('#celebrities').append('<li>' + celeb + '</li>');
+    $('#celebrity-count').html("Celebrities added: " + data.celebs.length);
   });
+});
 
-  socket.on('currentCelebs', function(data) {
-    data.celebs.forEach(function(celeb) {
-      $('#celebrities').append('<li>' + celeb + '</li>');
-    });
-  });
+socket.on('newCelebrity', function (data) {
+  $('#celebrities').append('<li>' + data.name + '</li>');
+  $('#celebrity-count').html("Celebrities added: " + data.count);
+});
 
-  socket.on('newCelebrity', function (data) {
-    $('#celebrities').append('<li>' + data.name + '</li>');
-  });
+$('#addCeleb').on('submit', function(e) {
+  var celebInput = $('#celebrity');
 
-  $('#addCeleb').on('submit', function(e) {
-    var celebInput = $('#celebrity');
+  e.preventDefault();
+  console.log(celebInput.val());
 
-    e.preventDefault();
+  socket.emit('addCeleb', { celeb: celebInput.val() });
 
-    socket.emit('addCeleb', { celeb: celebInput.val() });
+  // Clear input
+  celebInput.val('');
 
-    // Clear input
-    celebInput.val('');
-
-  });
-})();
+});
