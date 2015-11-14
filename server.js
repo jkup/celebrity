3e8cac96c81b935097b9b0579b7ab03d8e8247fc
Jon Kuperman <jon.kuperman@gmail.com>
2015-11-14 13:07:05 -0600

    moved sockets into its own file

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var app     = express();
 var server  = require('http').Server(app);
-var io      = require('socket.io')(server);
 
-// Application variables
-var game = {
-  totalUsers: 0,
-  rooms: {}
-}
+var game = { totalUsers: 0, rooms: {} };
+
+var sockets = require('./lib/sockets.js')(server, game);
 
 app.set('view engine', 'jade');
 app.use(express.static('public'));
@@ -67,52 +64,6 @@ app.get('/game/:gameId/admin', function (req, res) {
       error: 'That roooom doesn\'t exist.'
     });
   }
-});
-
-io.on('connection', function (socket) {
-  // New user connects, up user count and distribute
-  // new count to all clients
-  game.totalUsers++;
-  io.sockets.emit('totalUsers', { users: game.totalUsers });
-
-  // When a new celebrity is added, push new celebrity
-  // to all clients
-  socket.on('addCeleb', function (data) {
-    var celebToAdd = data.celeb;
-    var roomToAddTo = data.roomName;
-    var celebrities = game.rooms[roomToAddTo].celebrities;
-
-    celebrities.push(celebToAdd);
-    io.emit('newCelebrity', { name: celebToAdd, count: celebrities.length });
-  });
-
-  socket.on('newRoom', function(data) {
-    var roomName = data.roomName;
-
-    if (roomName in game.rooms) {
-      socket.emit('roomFailed', { message: 'That room already exists' });
-    } else {
-      game.rooms[roomName] = { currentUsers: 0, celebrities: [] };
-      socket.emit('roomCreated', { roomName: roomName });
-    }
-  });
-
-  socket.on('joinRoom', function(data) {
-    var roomName = data.roomName;
-
-    if (roomName in game.rooms) {
-      socket.emit('roomJoined', { roomName: roomName });
-    } else {
-      socket.emit('roomFailed', { message: 'That room doesn\'t exist' });
-    }
-  });
-
-  // User disconnects, lower user count and distribute
-  // new count to all clients
-  socket.on('disconnect', function() {
-    game.totalUsers--;
-    io.sockets.emit('totalUsers', { users: game.totalUsers });
-  });
 });
 
 server.listen(3000);
